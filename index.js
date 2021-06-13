bf50548575b57a44f9d7069a2cc1813e6db3a109
Ana Luísa <ana.luisa.fogarin@gmail.com>
2021-06-13 17:58:36 -0300

    Server for SI700 project

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const cors = require("cors")
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', function(req, res){res.send('Hello world')});
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+server.listen(process.env.PORT || 3000); 
+
+const endpoint = "/products";
+
+app.get(endpoint, function(req, res){
+    res.send(products.filter(Boolean));
+});
+
+app.get(`${endpoint}/:id`, function(req, res){
+    const id = req.params.id;
+    const product = products[id];
+
+    if (!product){
+        res.send("{}");
+    } else {
+        res.send(product);
+    }  
+});
+
+// Insert
+app.post(endpoint, (req, res) => {
+    const product = {
+        id : parseInt(products.length),
+        name : req.body["nameProduct"],
+        quantity : req.body["quantity"], 
+        type : req.body["type"], 
+        dateBuy: req.body["dateBuy"],
+        expirationDate: req.body["expirationDate"]
+    };
+    products.push(product);
+    res.send("1");
+
+    notify();
+});
+
+// Atualização
+app.put(`${endpoint}/:id`, (req, res) =>{
+    const id = parseInt(req.params.id);
+    const product = {
+        id : id,
+        name : req.body["name"],
+        quantity : req.body["quantity"], 
+        type : req.body["type"], 
+        dateBuy: req.body["dateBuy"],
+        expirationDate: req.body["expirationDate"]
+    };
+
+    products[id] = product;
+    res.send("1");
+
+    notify();
+});
+
+app.delete(`${endpoint}/:id`, (req, res) => {
+    const id = req.params.id;
+    delete products[id];
+    res.send("1");
+
+    notify();
+});
+
+/*
+  Criar um socket para notificar usuários das mudanças.
+*/
+
+
+// Comunicação
+const INVALIDATE = 'invalidate';
+
+function notify() {
+    io.sockets.emit(INVALIDATE, 1);
+}
+
+/* Servidor */
+
+const products = [
+    {id: 0, name: "Pinça", type: "Utensílio", quantity: 10, dateBuy : "2021-06-21", expirationDate : "-"},
+    {id: 1, name: "Henna", type: "Material", quantity: 10, dateBuy : "2021-06-21", expirationDate : "-"},
+    {id: 1, name: "Algodao", type: "Descartável", quantity: 100, dateBuy : "2021-06-21", expirationDate : "-"},
+]
